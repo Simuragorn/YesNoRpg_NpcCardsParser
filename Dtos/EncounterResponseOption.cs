8a2eb16c4cc27e4a9b5a94088823baf308222b44
Bogdan.G <bogdan.g@solity.io>
2025-03-05 16:40:43 +0200

    Remove unused modificator

--- Dtos/EncounterResponseOption.cs
+++ Dtos/EncounterResponseOption.cs
@@ -5,7 +5,7 @@ namespace CardsExcelParser.Dtos
     public class EncounterResponseOption
     {
         public NpcResponseOptionTypeEnum Type;
-        public required string ResponseText;
+        public string ResponseText;
         public int GoldDelta;
         public int MaterialsDelta;
         public int ReputationDelta;
